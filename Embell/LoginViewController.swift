c0344cd693b91a9b3d00e6067697e3db0ec3d293
Jonathon Vega <jfvega@live.com>
2017-03-10 01:49:06 -0500

    Initial Commit

---
+++ Embell/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Embell
+//
+//  Created by Jonathon F Vega on 3/9/17.
+//  Copyright © 2017 Jonathon Vega. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
