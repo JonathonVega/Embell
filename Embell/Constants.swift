9d5b723ed43c4221984bf637f9cff1cf28a8f3d2
Jonathon Vega <jfvega@live.com>
2017-03-12 15:00:14 -0400

    Added Constants and UserProfileVC

---
+++ Embell/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Embell
+//
+//  Created by Jonathon F Vega on 3/12/17.
+//  Copyright © 2017 Jonathon Vega. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    // DBProvider
+    static let CHATROOMS = "Chat Rooms"
+    static let MESSAGES = "Messages"
+    
+    static let EMAIL = "email"
+    static let PASSWORD = "password"
+    
+    // Messages
+    static let TEXT = "text"
+    static let SENDER_ID = "sender_id"
+    static let SENDER_NAME = "sender_name"
+    static let URL = "url"
+    
+}
---
+++ Embell/UserProfileViewController.swift
@@ -0,0 +1,50 @@
+//
+//  UserProfileViewController.swift
+//  Embell
+//
+//  Created by Jonathon F Vega on 3/12/17.
+//  Copyright © 2017 Jonathon Vega. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class UserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logOut(_ sender: UIButton) {
+        
+        if FIRAuth.auth()?.currentUser != nil {
+            print((FIRAuth.auth()?.currentUser?.email)!)
+            let firebaseAuth = FIRAuth.auth()
+            do {
+                try firebaseAuth?.signOut()
+                self.performSegue(withIdentifier: "signOutToLogin", sender: self)
+            } catch let signOutError as NSError{
+                print("Error signing out: %@", signOutError)
+            }
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
