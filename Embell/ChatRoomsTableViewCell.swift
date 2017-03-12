a29033d1a4d0b03656f8ac25f82938ce54beb7c7
Jonathon Vega <jfvega@live.com>
2017-03-12 00:01:48 -0500

    Added chatRoomsTableViewController and Cell

---
+++ Embell/ChatRoomsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ChatRoomsTableViewCell.swift
+//  Embell
+//
+//  Created by Jonathon F Vega on 3/11/17.
+//  Copyright © 2017 Jonathon Vega. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var chatRoomImage: UIImageView!
+    
+    @IBOutlet weak var chatRoomName: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
